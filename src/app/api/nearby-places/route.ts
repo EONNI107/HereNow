1fe8fdcbc46a0e4e553f5b682745d1be9c4bd3de
Maestrossa <s-i-s-e-u-n-g@hanmail.net>
2024-07-25 20:24:14 +0900

    update: 상세페이지 지도 추가

--- src/app/api/nearby-places/route.ts
+++ src/app/api/nearby-places/route.ts
@@ -4,13 +4,13 @@ import { tourApi } from '../tourApi';
 export const GET = async (request: Request) => {
   const serviceKey = process.env.NEXT_PUBLIC_TOURAPI_KEY;
   const urlParams = new URL(request.url).searchParams;
-  const xValue = urlParams.get('mapX');
-  const yValue = urlParams.get('mapY');
+  const latitude = urlParams.get('mapX');
+  const longitude = urlParams.get('mapY');
   const typeId = urlParams.get('typeId');
 
   try {
     const response = await tourApi.get(
-      `locationBasedList1?MobileOS=ETC&MobileApp=HereNow&_type=json&arrange=Q&mapX=${xValue}&mapY=${yValue}&radius=2000&contentTypeId=${typeId}&serviceKey=${serviceKey}
+      `locationBasedList1?numOfRows=10&MobileOS=ETC&MobileApp=HereNow&_type=json&arrange=Q&mapX=${latitude}&mapY=${longitude}&radius=2000&contentTypeId=${typeId}&serviceKey=${serviceKey}
     `,
     );
     return NextResponse.json(response.data);
