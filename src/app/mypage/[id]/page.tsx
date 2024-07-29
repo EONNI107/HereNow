21cef079fa92543cbfd8ecb5c68a487827f685be
Leeseayoung <tnfusrltk12@naver.com>
2024-07-29 12:58:21 +0900

    update: 이미지 업로드,미리보기,닉네임수정 기능 추가

--- src/app/mypage/[id]/page.tsx
+++ src/app/mypage/[id]/page.tsx
@@ -1,8 +1,8 @@
 'use client';
 
 import { createClient } from '@/utils/supabase/client';
-import { showToast } from '@/utils/toastHelper';
-import { useEffect, useState } from 'react';
+import { ChangeEvent, useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
 
 function MyPage() {
   const supabase = createClient();
@@ -15,19 +15,19 @@ function MyPage() {
   const [isEditing, setIsEditing] = useState(false);
   const [editProfile, setEditProfile] = useState({
     nickname: '',
-    email: '',
     profileImage: '',
   });
+  const [newImageFile, setNewImageFile] = useState<File | null>(null);
+  const [imagePreview, setImagePreview] = useState<string | null>(null);
 
   useEffect(() => {
     const fetchProfile = async () => {
       const user = await supabase.auth.getUser();
-      console.log(user);
-      if (user) {
+      if (user.data.user) {
         const { data, error } = await supabase
           .from('Users')
           .select('nickname, email, profileImage')
-          .eq('id', user.data.user?.id)
+          .eq('id', user.data.user.id)
           .single();
 
         if (error) {
@@ -36,7 +36,6 @@ function MyPage() {
           setProfile(data);
           setEditProfile({
             nickname: data.nickname,
-            email: data.email,
             profileImage: data.profileImage,
           });
         }
@@ -46,24 +45,60 @@ function MyPage() {
     fetchProfile();
   }, []);
 
-  const handleSave = async () => {
+  const handleImageChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files[0]) {
+      setNewImageFile(e.target.files[0]);
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setImagePreview(reader.result as string);
+      };
+      reader.readAsDataURL(e.target.files[0]);
+    }
+  };
+
+  const uploadImage = async (file: File) => {
+    const { data, error } = await supabase.storage
+      .from('profileImage')
+      .upload(`avatar_${Date.now()}.png`, file);
+
+    if (error) {
+      console.error('Error uploading file:', error.message);
+      return null;
+    }
+
+    return data.path;
+  };
+
+  const handleUpdate = async () => {
     const user = await supabase.auth.getUser();
-    if (user) {
+    if (user.data.user) {
+      let imagePath = profile.profileImage;
+
+      if (newImageFile) {
+        const path = await uploadImage(newImageFile);
+        if (path) {
+          imagePath = `https://cuxcqeqwbwfuxipnozwy.supabase.co/storage/v1/object/public/profileImage/${path}`;
+        }
+      }
+
       const { error } = await supabase
         .from('Users')
         .update({
           nickname: editProfile.nickname,
-          email: editProfile.email,
-          profileImage: editProfile.profileImage,
+          profileImage: imagePath,
         })
-        .eq('id', user.data.user?.id);
+        .eq('id', user.data.user.id);
 
       if (error) {
         console.error('에러', error.message);
       } else {
-        setProfile(editProfile);
+        setProfile((prev) => ({
+          ...prev,
+          nickname: editProfile.nickname,
+          profileImage: imagePath,
+        }));
         setIsEditing(false);
-        showToast('success', '수정이 완료되었습니다');
+        toast.success('프로필 수정이 완료되었습니다.');
       }
     }
   };
@@ -73,23 +108,18 @@ function MyPage() {
       <div>마이 페이지</div>
       <div className="flex items-center bg-cyan-200 rounded-md w-full m-10">
         <img
-          src="https://search.pstatic.net/common/?src=http%3A%2F%2Fshopping.phinf.naver.net%2Fmain_4390239%2F43902391618.20231108175120.jpg&type=a340"
-          className="h-12 m-3 rounded-full"
+          src={
+            imagePreview ||
+            profile.profileImage ||
+            'https://via.placeholder.com/150'
+          }
+          className="h-12 w-12 m-3 rounded-full"
+          alt="Profile"
         />
         <div>
           {isEditing ? (
             <>
-              <div className="mt-5">
-                email:
-                <input
-                  type="text"
-                  value={editProfile.email}
-                  onChange={(e) =>
-                    setEditProfile({ ...editProfile, email: e.target.value })
-                  }
-                  className="ml-2 p-1 border rounded"
-                />
-              </div>
+              <div className="mt-5">email: {profile.email}</div>
               <div className="mb-5">
                 닉네임:
                 <input
@@ -101,20 +131,25 @@ function MyPage() {
                   className="ml-2 p-1 border rounded"
                 />
               </div>
+              <div>
+                프로필 이미지:
+                <input
+                  type="file"
+                  onChange={handleImageChange}
+                  className="ml-2 p-1 border rounded"
+                />
+              </div>
             </>
           ) : (
             <>
               <div className="mt-5">email: {profile.email}</div>
-              <div className="mb-5">
-                닉네임:
-                {profile.nickname}
-              </div>
+              <div className="mb-5">닉네임: {profile.nickname}</div>
             </>
           )}
         </div>
         {isEditing ? (
           <button
-            onClick={handleSave}
+            onClick={handleUpdate}
             className="m-2 w-40 rounded-md bg-sky-500"
           >
             수정 완료
@@ -122,7 +157,7 @@ function MyPage() {
         ) : (
           <button
             onClick={() => setIsEditing(true)}
-            className="m-5 w-30  rounded-md bg-sky-500"
+            className="m-5 w-30 rounded-md bg-sky-500"
           >
             프로필 수정
           </button>
