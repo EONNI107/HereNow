40bfa822eb54a18ac92b4f009addcb9e8a8a20f8
junghyunwoo02 <jungilbung@naver.com>
2024-07-30 22:03:17 +0900

    feat: Feeds 테이블 데이터 가져와서 화면에 렌더링, 필요없는 css코드 삭제 dayjs 유틸 함수 수정

--- src/app/(feed)/feed/page.tsx
+++ src/app/(feed)/feed/page.tsx
@@ -1,9 +1,9 @@
 import FeedListItem from '@/components/FeedList/FeedListItem';
-import { Feed } from '@/types/feed';
 import { createClient } from '@/utils/supabase/server';
 import { showToast } from '@/utils/toastHelper';
+import Image from 'next/image';
 
-async function Page() {
+async function FeedListPage() {
   const supabase = createClient();
   const { data, error } = await supabase.from('Feeds').select('*');
 
@@ -16,12 +16,38 @@ async function Page() {
 
   return (
     <div>
-      <h1>Feeds</h1>
-      <div className="grid grid-cols-1 gap-4">
-        {data && data.map((feed) => <FeedListItem key={feed.id} feed={feed} />)}
+      <div className="relative mb-4">
+        <div className="aspect-[16/9] w-full">
+          <Image
+            src={'/AreaDetail-Main.jpg'}
+            alt="피드 상단 이미지"
+            width={300}
+            height={200}
+            priority={true}
+            className="object-cover w-full"
+          />
+        </div>
+        <div className="absolute inset-0 bg-black bg-opacity-15 flex flex-col justify-end p-8">
+          <div className="flex flex-col space-y-2">
+            <button className="self-start font-semibold bg-blue-500 text-white px-4 py-2 rounded-md">
+              글쓰러 가기
+            </button>
+            <h1 className="font-semibold text-[28px] text-white">
+              <span className="block">홍길동님만의</span>
+              맛집 · 여행지를 공유해주세요!
+            </h1>
+          </div>
+        </div>
+      </div>
+      <div className="container mx-auto p-4">
+        <div className="mb-4">{/* 드롭다운 구현 */}</div>
+        <div className="grid grid-cols-1 gap-4">
+          {data &&
+            data.map((feed) => <FeedListItem key={feed.id} feed={feed} />)}
+        </div>
       </div>
     </div>
   );
 }
 
-export default Page;
+export default FeedListPage;
--- src/components/FeedList/FeedListItem.tsx
+++ src/components/FeedList/FeedListItem.tsx
@@ -1,20 +1,43 @@
 import React from 'react';
 import { Feed } from '@/types/feed';
 import Image from 'next/image';
+import { fromNow } from '@/utils/formatDate';
 
 function FeedListItem({ feed }: { feed: Feed }) {
   return (
     <div className="p-4 border rounded-3xl">
-      <Image
-        src={`${feed.image}`}
-        alt="피드 썸네일"
-        width={300}
-        height={200}
-        className="object-cover w-full h-48 border rounded-3xl"
-      />
-      <h2 className="text-xl font-semibold">{feed.title}</h2>
-      <p className="text-gray-600">{feed.content}</p>
-      <p className="text-sm text-gray-400">{feed.createdAt}</p>
+      <div className="flex items-center mb-4">
+        <Image
+          src={'/'}
+          alt="유저 아바타"
+          width={100}
+          height={100}
+          className="w-12 h-12 border rounded-full mr-4"
+        />
+        <span className="text-[14px] font-regular">닉네임</span>
+      </div>
+      <div className="relative h-48">
+        <Image
+          src={`/${feed.image}`}
+          alt="피드 썸네일"
+          width={300}
+          height={200}
+          className="object-cover w-full h-48 border rounded-3xl"
+        />
+      </div>
+      <div className="p-4">
+        <h2 className="text-[#212125] text-[18px] font-semibold mb-2">
+          {feed.title}
+        </h2>
+        <p className="text-[14px] text-[#767676] mb-2">{feed.content}</p>
+        <div className="flex items-center justify-between text-xs text-[#505050]">
+          <span>{fromNow(feed.createdAt)}</span>
+          <div className="flex items-center space-x-2">
+            <span>좋아요 0</span>
+            <span>댓글 0</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
