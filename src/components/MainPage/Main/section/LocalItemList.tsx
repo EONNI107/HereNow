049c0360ee21f0c4253c43fb2965dd1911657398
show1486 <show1486@naver.com>
2024-08-01 15:52:47 +0900

    move:파일 이름 정정

--- src/components/MainPage/Main/section/LocalItemList.tsx
+++ src/components/MainPage/Main/section/LocalItemList.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { Regions } from '@/types/maintype';
+import { Regions } from '@/types/mainType';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 function LocalItemList() {
