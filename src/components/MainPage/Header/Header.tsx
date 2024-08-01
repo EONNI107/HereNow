58a84701a3125f677123fceeba06ac6a6d67130f
show1486 <show1486@naver.com>
2024-08-01 16:47:54 +0900

    comment: 주석제거

--- src/components/MainPage/Header/Header.tsx
+++ src/components/MainPage/Header/Header.tsx
@@ -37,14 +37,3 @@ function Header({ title, content }: DataProps) {
   );
 }
 export default Header;
-
-// <div className="h-[200px] w-full flex flex-col items-center relative">
-//       <Image src="/backgroundimage.png" alt="메인 상단 이미지" fill priority />
-//       <div className="text-white absolute bottom-10 left-8">
-//         <h2>{title}</h2>
-//         <p>{content}</p>
-//       </div>
-//       <button className="absolute bottom-1 left-8 text-white bg-[#ccc]">
-//         공유하러가기
-//       </button>
-//     </div>
