0e1c9701a384ceadc774fb2021fb7daf8cbf1b2e
Maestrossa <s-i-s-e-u-n-g@hanmail.net>
2024-08-07 16:35:53 +0900

    fix: 카카오맵 위도&경도 예외처리

--- src/components/MainPage/search/SearchFilterItem.tsx
+++ src/components/MainPage/search/SearchFilterItem.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { SearchedType } from '@/app/search-page/page';
+import { SearchedType } from '@/app/(layout)/search-page/page';
 import { HeartIcon } from '@heroicons/react/24/outline';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
